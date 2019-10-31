a98155e8f956b625bd9d53012ba807df989e4f5c
juzoh-morioka <juzoh.armor@gmail.com>
2019-11-01 00:58:52 +0900

    午前一時

---
+++ hangman.py
@@ -0,0 +1,44 @@
+#これはハングマンという文字当てゲーム 一文字ずつ入力して
+
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "______     ",
+              "|          ",
+              "|     |    ",
+              "|     0    ",
+              "|    /|\   ",
+              "|    / \   ",
+              "|          "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman!")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+        
+#ここの「if not win:」というコードをtabキー２つの空白だとそれ以降の２行は実行されなかった
+        
+    if not win:
+            print("\n".join(stages[0:wrong]))
+            print("You lose! It was {}.".format(word))
+
+hangman("cat")
